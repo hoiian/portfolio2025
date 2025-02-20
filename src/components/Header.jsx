a1339dff9b0dd5b5ca815497a42dfbdd7f92cb5a
hoiian <hoiian96@gmail.com>
2025-02-20 17:07:09 +0800

    add reserve color for header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-1/2 w-[1360px] transform -translate-x-1/2 h-[80px] flex items-center justify-end z-50">
+    <header className="fixed top-0 left-1/2 w-[1360px] transform -translate-x-1/2 h-[80px] flex items-center justify-end z-50 mix-blend-difference">
       <nav className="flex space-x-6">
         <NavLink 
           to="/" 
