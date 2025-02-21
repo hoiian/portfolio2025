fce00988dbac19ffbdbd58cdd171c4cc701c918d
hoiian <hoiian96@gmail.com>
2025-02-21 11:05:28 +0800

    update transition

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -8,15 +8,28 @@ export default function PageTransition({ children }) {
 
   return (
     <AnimatePresence mode="wait">
+      {/* Fade In */}
       <motion.div
         key={location.pathname}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.2 }}
+        transition={{ duration: 0.15 }}
       >
         {children}
       </motion.div>
+
+      {/* Slide In from Bottom */}
+      {/* <motion.div
+        key={location.pathname}
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -100, opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {children}
+      </motion.div> */}
+
     </AnimatePresence>
   );
 }
