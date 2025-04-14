3a1aca9a27cd298d391c0d7d1287b55c797e323b
hoiian <hoiian96@gmail.com>
2025-04-14 22:58:40 +0800

    resume scroll x

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-1/2 max-w-[1360px] w-full transform -translate-x-1/2 h-[80px] flex items-center justify-end z-50 mix-blend-difference p-5 md:p-0">
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 h-[80px] max-w-[1360px] w-full  flex items-center justify-end z-50 mix-blend-difference p-5 md:p-0">
       <nav className="flex space-x-6">
         <NavLink
           to="/"
