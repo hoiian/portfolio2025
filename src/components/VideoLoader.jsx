24e3bd31f171b098e4f7e140913418aed44e22fd
hoiian <hoiian96@gmail.com>
2025-04-17 23:36:03 +0800

    loading

---
+++ src/components/VideoLoader.jsx
@@ -0,0 +1,18 @@
+// src/components/VideoLoader.jsx
+import React from "react";
+import loaderVideo from "/videos/loading.webm"; // 或 .webm
+
+export default function VideoLoader() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
+      <video
+        src={loaderVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className=" object-contain"
+      />
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,62 +1,67 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; //
-import Resume from "./pages/Resume";
-import Work1 from "./pages/Work1";
-import Work2 from "./pages/Work2";
-import Work3 from "./pages/Work3";
-import Footer from "./components/Footer";
 import PageTransition from "./components/PageTransition";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import VideoLoader from "./components/VideoLoader";
+
+// 改成 lazy load
+const Home = lazy(() => import("./pages/Home"));
+const Resume = lazy(() => import("./pages/Resume"));
+const Work1 = lazy(() => import("./pages/Work1"));
+const Work2 = lazy(() => import("./pages/Work2"));
+const Work3 = lazy(() => import("./pages/Work3"));
+const Footer = lazy(() => import("./components/Footer"));
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PageTransition>
-              <Home />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/resume"
-          element={
-            <PageTransition>
-              <Resume />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/work1"
-          element={
-            <PageTransition>
-              <Work1 />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/work2"
-          element={
-            <PageTransition>
-              <Work2 />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/work3"
-          element={
-            <PageTransition>
-              <Work3 />
-            </PageTransition>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<VideoLoader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PageTransition>
+                <Home />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/resume"
+            element={
+              <PageTransition>
+                <Resume />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/work1"
+            element={
+              <PageTransition>
+                <Work1 />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/work2"
+            element={
+              <PageTransition>
+                <Work2 />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/work3"
+            element={
+              <PageTransition>
+                <Work3 />
+              </PageTransition>
+            }
+          />
+        </Routes>
+      </Suspense>
     </Router>
     <Footer />
     <SpeedInsights />
